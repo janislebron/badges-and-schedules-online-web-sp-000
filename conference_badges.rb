576de623dfaa6160818de69a6a601c97b8fcbb0a
Janis Lebron <janislebron@gmail.com>
2020-05-17 19:02:48 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,10 +1,11 @@
-speakers = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
+
 
 def badge_maker(name)
   puts "Hello, my name is #{name}."
 end
 
 def batch_badge_creator(speakers)
+speakers = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
   speakers.collect do |name|
     badge_maker(name)
   end
