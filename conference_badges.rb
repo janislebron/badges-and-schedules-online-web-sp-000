de6495385629062ba8cead1ff054faadd3227046
Janis Lebron <janislebron@gmail.com>
2020-05-17 18:26:29 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,17 @@
-# Write your code here.
+def badge_maker(name)
+  puts "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(speakers)
+speaker =  ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
+return badge_maker(speaker)
+end
+
+def assign_rooms(speaker, room)
+speaker =  ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
+room = [1, 2, 3, 4, 5, 6, 7]
+puts "Hello, #{name}! You'll be assigned to room #{room}!"
+end
+
+def printer
+end
